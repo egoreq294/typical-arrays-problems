8f1c1454398cf47508c9032b53c1d074c2bb4267
egoreq <egoreq@bk.ru>
2020-02-20 21:08:11 +0400

    feat: passed tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+  if(array === undefined || array.length === 0)
   return 0;
+  let min = Infinity;
+  for(let i in array){
+    if (array[i]<=min){
+    min = array[i]
+    }
+  }
+  return min;  
 }
 
 exports.max = function max (array) {
+  if(array === undefined || array.length === 0)
   return 0;
+  let min = -Infinity;
+  for(let i in array){
+    if (array[i]>=min){
+    min = array[i]
+    }
+  }
+  return min;  
 }
 
 exports.avg = function avg (array) {
+  let avg = 0;
+  if(array === undefined || array.length === 0)
   return 0;
+  for(let i in array){
+    avg+=array[i];
+  }
+  return avg/array.length;
 }
